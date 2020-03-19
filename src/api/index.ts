e4e9bddb237730917617f5dc19a3387ea2ba121a
Jonas Keinholz <git@inyono.dev>
2020-03-19 10:56:10 +0100

    api: various fixes, add playground

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,27 +5,31 @@ import { Request as ApolloServerRequest } from 'apollo-server-env/dist/fetch'
 
 import { SerloDataSource } from './data-sources/serlo'
 import { typeDefs, resolvers } from './schema'
+import { graphiql } from './graphiql'
 
 export async function api(request: Request) {
-  if (
-    getSubdomain(request.url) !== 'api' ||
-    getPathname(request.url) !== '/graphql'
-  ) {
-    return null
+  if (getSubdomain(request.url) !== 'api') return null
+
+  if (getPathname(request.url) === '/graphql') {
+    if (request.method === 'OPTIONS') {
+      const response = new Response('', { status: 204 })
+      setCorsHeaders(response)
+      return response
+    }
+
+    const server = createGraphQLServer()
+    const apolloServerRequest = (request as unknown) as ApolloServerRequest
+    const apolloServerResponse = await graphqlCloudflare(() => {
+      return server.createGraphQLServerOptions(apolloServerRequest)
+    })(apolloServerRequest)
+    const response = (apolloServerResponse as unknown) as Response
+    setCorsHeaders(response)
+    return response
   }
 
-  if (request.method === 'OPTIONS') {
-    return new Response('', { status: 204 })
+  if (getPathname(request.url) === '/___graphql') {
+    return new Response(graphiql, { headers: { 'Content-Type': 'text/html' } })
   }
-
-  const server = createGraphQLServer()
-  const apolloServerRequest = (request as unknown) as ApolloServerRequest
-  const apolloServerResponse = await graphqlCloudflare(() => {
-    return server.createGraphQLServerOptions(apolloServerRequest)
-  })(apolloServerRequest)
-  const response = (apolloServerResponse as unknown) as Response
-  setCorsHeaders(response)
-  return response
 }
 
 export function createGraphQLServer() {
@@ -43,10 +47,7 @@ export function createGraphQLServer() {
 
 function setCorsHeaders(response: Response) {
   response.headers.set('Access-Control-Allow-Credentials', 'true')
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    'application/json, Content-type'
-  )
+  response.headers.set('Access-Control-Allow-Headers', 'Content-type')
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
   response.headers.set('Access-Control-Allow-Origin', '*')
   response.headers.set('X-Content-Type-Options', 'nosniff')
