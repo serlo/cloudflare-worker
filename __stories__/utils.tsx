69c9325cd54cabb0f39ef70e83b2a8250c405599
Stephan Kulla <git.mail@kulla.me>
2020-03-22 19:31:25 +0100

    storybook: Apply suggestions from code review
    Co-Authored-By: Jonas Keinholz <git@inyono.dev>

--- __stories__/utils.tsx
+++ __stories__/utils.tsx
@@ -1,10 +1,10 @@
 import { h } from 'preact'
-import { FunctionComponent, RenderableProps, VNode } from 'preact'
+import { ComponentType, RenderableProps } from 'preact'
 import renderToString from 'preact-render-to-string'
 
-export function createStaticComponent<P = {}>(Component: FunctionComponent<P>) {
+export function createStaticComponent<P = {}>(Component: ComponentType<P>) {
   return function StaticComponent(props: RenderableProps<P>) {
-    const html = renderToString(Component(props) as VNode<any>)
+    const html = renderToString(<Component {...props} />)
 
     return <div dangerouslySetInnerHTML={{ __html: html }} />
   }
