9cd405eb5ce2b827f5409f9fa368d1033e574124
Stephan Kulla <git.mail@kulla.me>
2020-04-22 10:37:21 +0200

    frontend: Check for type of requests path

--- __tests__/frontend-proxy.ts
+++ __tests__/frontend-proxy.ts
@@ -1,11 +1,15 @@
-import { handleRequest, FRONTEND_DOMAIN } from '../src/frontend-proxy'
+import {
+  handleRequest,
+  FRONTEND_DOMAIN,
+  createApiQuery,
+} from '../src/frontend-proxy'
+import { createJsonResponse } from '../src/utils'
 import { withMockedFetch, hasOkStatus } from './utils'
 
-describe('handleRequest()', () => {
-  async function handleUrl(url: string, probability = 0.1) {
-    return (await handleRequest(new Request(url), probability)) as Response
-  }
+const TEST_ALLOWED_TYPES = ['User', 'Article']
+const testUrl = 'https://de.serlo.org/example-url'
 
+describe('handleRequest()', () => {
   describe('returns null if language tenant is not "de"', () => {
     test.each([
       'https://serlo.org/',
@@ -19,22 +23,40 @@ describe('handleRequest()', () => {
 
   describe('chooses backend based on probability', () => {
     test.each([
-      [1, 'frontend', true],
-      [0, 'no-frontend', false],
+      [1, TEST_ALLOWED_TYPES[0], 'frontend', true],
+      [1, TEST_ALLOWED_TYPES[1], 'frontend', true],
+      [0, TEST_ALLOWED_TYPES[0], 'no-frontend', false],
+      [0, TEST_ALLOWED_TYPES[1], 'no-frontend', false],
     ])(
       'probability=%p',
-      async (probability, responseText, useFrontendCookie) => {
-        await withMockedFetch(checkFrontendRequest, async () => {
-          const req = new Request('https://de.serlo.org/example')
-          const res = (await handleRequest(req, probability)) as Response
+      async (probability, typename, responseText, useFrontendCookie) => {
+        await withMockedFetch(
+          [createApiResponse(typename), checkFrontendRequest],
+          async () => {
+            const res = await handleUrl(testUrl, probability)
 
-          expect(await res.text()).toBe(responseText)
-          expect(res.headers.get('Set-Cookie')).toBe(
-            `useFrontend${probability * 100}=${useFrontendCookie}; path=/`
-          )
-        })
+            expect(await res.text()).toBe(responseText)
+            expect(res.headers.get('Set-Cookie')).toBe(
+              `useFrontend${probability * 100}=${useFrontendCookie}; path=/`
+            )
+          }
+        )
       }
     )
+  })
+
+  describe('returns null for not allowed taxonomy types', () => {
+    test.each(['Page', 'TaxonomyTerm'])('typename=%p', async (typename) => {
+      await withMockedFetch([createApiResponse(typename)], async () => {
+        expect(await handleUrl(testUrl, 1)).toBeNull()
+      })
+    })
+  })
+
+  test('returns null for unknown paths', async () => {
+    await withMockedFetch([createApiErrorResponse()], async () => {
+      expect(await handleUrl(testUrl, 1)).toBeNull()
+    })
   })
 
   describe('requests to /_next always resolve to frontend', () => {
@@ -42,7 +64,7 @@ describe('handleRequest()', () => {
       'https://de.serlo.org/_next/script.js',
       'https://de.serlo.org/_next/img/picture.svg',
     ])('URL is %p', async (url) => {
-      await withMockedFetch(checkFrontendRequest, async () => {
+      await withMockedFetch([checkFrontendRequest], async () => {
         const response = await handleUrl(url)
 
         expect(await response.text()).toBe('frontend')
@@ -52,37 +74,51 @@ describe('handleRequest()', () => {
   })
 
   test('uses frontend when it is enabled via cookie', async () => {
-    await withMockedFetch(checkFrontendRequest, async () => {
-      const request = new Request('https://de.serlo.org/example')
-      request.headers.set('Cookie', 'useFrontend75=true;')
+    await withMockedFetch(
+      [createApiResponse(), checkFrontendRequest],
+      async () => {
+        const res = await handleUrl(testUrl, 0, 'useFrontend0=true;')
 
-      const response = (await handleRequest(request, 0.75)) as Response
-      expect(response.headers.get('Set-Cookie')).toBeNull()
-      expect(await response.text()).toBe('frontend')
-    })
+        expect(res.headers.get('Set-Cookie')).toBeNull()
+        expect(await res.text()).toBe('frontend')
+      }
+    )
   })
 
   test('do not use fronted when it is disabled via cookie', async () => {
-    await withMockedFetch(checkFrontendRequest, async () => {
-      const request = new Request('https://de.serlo.org/example')
-      request.headers.set('Cookie', 'useFrontend75=false;')
+    await withMockedFetch(
+      [createApiResponse(), checkFrontendRequest],
+      async () => {
+        const res = await handleUrl(testUrl, 1, 'useFrontend100=false;')
 
-      const response = (await handleRequest(request, 0.75)) as Response
-      expect(response.headers.get('Set-Cookie')).toBeNull()
-      expect(await response.text()).toBe('no-frontend')
+        expect(res.headers.get('Set-Cookie')).toBeNull()
+        expect(await res.text()).toBe('no-frontend')
+      }
+    )
+  })
+
+  test('creates new response object for calls to frontend', async () => {
+    const frontendResponse = new Response('')
+
+    await withMockedFetch([createApiResponse(), frontendResponse], async () => {
+      const res = await handleUrl(testUrl, 1)
+      expect(res).not.toBe(frontendResponse)
     })
   })
 
   describe('transfers request meta data to backend', () => {
     test.each([true, false])('useFrontend=%p', async (useFrontend) => {
-      await withMockedFetch(checkRequestData, async () => {
-        const request = new Request('https://de.serlo.org/example')
-        request.headers.set('Cookie', `useFrontend20=${useFrontend};`)
-        request.headers.set('X-Header', 'foo')
+      await withMockedFetch(
+        [createApiResponse(), checkRequestData],
+        async () => {
+          const cookie = `useFrontend20=${useFrontend};`
+          const response = await handleUrl(testUrl, 0.2, cookie, {
+            headers: { 'X-Header': 'foo' },
+          })
 
-        const response = (await handleRequest(request, 0.2)) as Response
-        expect(await response.text()).toBe('header-shown')
-      })
+          expect(await response.text()).toBe('header-shown')
+        }
+      )
 
       function checkRequestData(req: Request) {
         const res =
@@ -97,18 +133,15 @@ describe('handleRequest()', () => {
 
   describe('transfers response meta data from backend', () => {
     test.each([true, false])('useFrontend=%p', async (useFrontend) => {
-      const targetResponse = new Response('hello', {
-        headers: {
-          'X-Header': 'bar',
-        },
+      const targetResponse = new Response('', {
+        headers: { 'X-Header': 'bar' },
       })
 
-      await withMockedFetch(targetResponse, async () => {
-        const request = new Request('https://de.serlo.org/example')
-        request.headers.set('Cookie', `useFrontend20=${useFrontend};`)
+      await withMockedFetch([createApiResponse(), targetResponse], async () => {
+        const cookie = `useFrontend20=${useFrontend};`
+        const res = await handleUrl(testUrl, 0.2, cookie)
 
-        const response = (await handleRequest(request, 0.2)) as Response
-        expect(response.headers.get('X-Header')).toBe('bar')
+        expect(res.headers.get('X-Header')).toBe('bar')
       })
     })
   })
@@ -131,6 +164,44 @@ describe('handleRequest()', () => {
     expect(await res.text()).toBe('Disable frontend')
   })
 })
+
+test('createApiQuery()', () => {
+  expect(createApiQuery('/math')).toBe(
+    '{ uuid(alias: { instance: de, path: "/math" }) { __typename } }'
+  )
+  expect(createApiQuery('hello')).toBe(
+    '{ uuid(alias: { instance: de, path: "/hello" }) { __typename } }'
+  )
+  expect(createApiQuery('/266/')).toBe(
+    '{ uuid(alias: { instance: de, path: "/266/" }) { __typename } }'
+  )
+  expect(createApiQuery('/266')).toBe('{ uuid(id: 266) { __typename } }')
+  expect(createApiQuery('/874329')).toBe('{ uuid(id: 874329) { __typename } }')
+})
+
+async function handleUrl(
+  url: string,
+  probability = 0.1,
+  cookie?: string,
+  reqInit?: RequestInit
+) {
+  const req = new Request(url, reqInit)
+
+  if (cookie !== undefined) req.headers.set('Cookie', cookie)
+
+  return (await handleRequest(req, probability, TEST_ALLOWED_TYPES)) as Response
+}
+
+function createApiResponse(typename = TEST_ALLOWED_TYPES[0]) {
+  return createJsonResponse({ data: { uuid: { __typename: typename } } })
+}
+
+function createApiErrorResponse() {
+  return createJsonResponse({
+    errors: [{ message: 'error' }],
+    data: { uuid: null },
+  })
+}
 
 function checkFrontendRequest(req: Request) {
   const res =
--- src/frontend-proxy.ts
+++ src/frontend-proxy.ts
@@ -1,57 +1,84 @@
 import { getSubdomain, getPathname } from './url-utils'
 
 export const FRONTEND_DOMAIN = 'frontend-sooty-ten.now.sh'
+const API_ENDPOINT = 'https://api.serlo.org/graphql'
+const FRONTEND_ALLOWED_TYPES = ['Article', 'TaxonomyTerm']
 const FRONTEND_PROBABILITY = 0.1
 
 export async function handleRequest(
   request: Request,
-  probability = FRONTEND_PROBABILITY
+  probability = FRONTEND_PROBABILITY,
+  allowdTypes = FRONTEND_ALLOWED_TYPES
 ): Promise<Response | null> {
   const url = request.url
   const path = getPathname(url)
 
   if (getSubdomain(url) !== 'de') return null
+
   if (path === '/enable-frontend')
     return createResponse('Enable frontend', probability, true)
   if (path === '/disable-frontend')
     return createResponse('Disable frontend', probability, false)
 
-  const { useFrontend, setCookie } = chooseBackend(request, probability)
+  if (path.startsWith('/_next')) return await fetchBackend(true, false)
 
-  const backendRequest = useFrontend
-    ? new Request(`https://${FRONTEND_DOMAIN}${path}`, request)
-    : request
+  const typename = await queryTypename(path)
+  if (typename === null || !allowdTypes.includes(typename)) return null
 
-  return createResponse(
-    await fetch(backendRequest),
-    probability,
-    setCookie ? useFrontend : undefined
-  )
-}
-
-function chooseBackend(
-  req: Request,
-  probability: number
-): { useFrontend: boolean; setCookie: boolean } {
-  const path = getPathname(req.url)
-  const cookies = req.headers.get('Cookie')
-
-  if (path.startsWith('/_next')) return { useFrontend: true, setCookie: false }
+  const cookies = request.headers.get('Cookie')
 
   if (cookies?.includes(formatCookie(true, probability)))
-    return { useFrontend: true, setCookie: false }
+    return fetchBackend(true, false)
   if (cookies?.includes(formatCookie(false, probability)))
-    return { useFrontend: false, setCookie: false }
+    return await fetchBackend(false, false)
 
-  return { useFrontend: Math.random() < probability, setCookie: true }
+  return await fetchBackend(Math.random() < probability, true)
+
+  async function fetchBackend(useFrontend: boolean, setCookie?: boolean) {
+    const backendRequest = useFrontend
+      ? new Request(
+          `https://${FRONTEND_DOMAIN}${getPathname(request.url)}`,
+          request
+        )
+      : request
+
+    return await createResponse(
+      await fetch(backendRequest),
+      probability,
+      setCookie ? useFrontend : undefined
+    )
+  }
 }
 
-function createResponse(
-  text: string | Response,
+async function queryTypename(path: string): Promise<string | null> {
+  const apiRequest = new Request(API_ENDPOINT, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: createApiQuery(path) }),
+  })
+  const apiResponse = await fetch(apiRequest)
+  const apiResult = await apiResponse.json()
+
+  return apiResult?.data?.uuid?.__typename ?? null
+}
+
+export function createApiQuery(path: string): string {
+  const pathWithoutSlash = path.startsWith('/') ? path.slice(1) : path
+  const query = /^\/?\d+$/.test(pathWithoutSlash)
+    ? `id: ${pathWithoutSlash}`
+    : `alias: { instance: de, path: "/${pathWithoutSlash}" }`
+
+  return `{ uuid(${query}) { __typename } }`
+}
+
+async function createResponse(
+  resInfo: string | Response,
   probability: number,
   futureFrontendUse?: boolean
 ) {
-  const response = typeof text === 'string' ? new Response(text) : text
+  const body = typeof resInfo === 'string' ? resInfo : await resInfo.text()
+  const init = typeof resInfo === 'string' ? undefined : resInfo
+  const response = new Response(body, init)
 
   if (futureFrontendUse !== undefined) {
     const cookie = `${formatCookie(futureFrontendUse, probability)}; path=/`
