16f8f5b39d8a052fac6a6988cb8878bcfe250323
Jonas Keinholz <git@inyono.dev>
2020-07-09 06:19:24 +0200

    auth: allow auth from localhost for dev & staging env

--- __tests__/auth.ts
+++ __tests__/auth.ts
@@ -1,7 +1,8 @@
 import { handleRequest } from '../src'
 import { expectIsJsonResponse } from './_helper'
 
-test('Frontend Sector Identifier URI Validation', async () => {
+test('Frontend Sector Identifier URI Validation (block localhost)', async () => {
+  global.ALLOW_AUTH_FROM_LOCALHOST = 'false'
   global.DOMAIN = 'serlo.org'
   const response = await handleRequest(
     new Request('https://serlo.org/auth/frontend-redirect-uris.json')
@@ -13,5 +14,22 @@ test('Frontend Sector Identifier URI Validation', async () => {
     'https://fr.serlo.org/api/auth/callback',
     'https://hi.serlo.org/api/auth/callback',
     'https://ta.serlo.org/api/auth/callback',
+  ])
+})
+
+test('Frontend Sector Identifier URI Validation (allow localhost)', async () => {
+  global.ALLOW_AUTH_FROM_LOCALHOST = 'true'
+  global.DOMAIN = 'serlo.org'
+  const response = await handleRequest(
+    new Request('https://serlo.org/auth/frontend-redirect-uris.json')
+  )
+  await expectIsJsonResponse(response, [
+    'https://de.serlo.org/api/auth/callback',
+    'https://en.serlo.org/api/auth/callback',
+    'https://es.serlo.org/api/auth/callback',
+    'https://fr.serlo.org/api/auth/callback',
+    'https://hi.serlo.org/api/auth/callback',
+    'https://ta.serlo.org/api/auth/callback',
+    'http://localhost:3000/api/auth/callback',
   ])
 })
