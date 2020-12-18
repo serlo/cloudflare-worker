16793b17f8da9160026e0706759ec6eee9b1583d
Stephan Kulla <git.mail@kulla.me>
2020-12-18 11:53:44 +0100

    Update __tests__/redirects.ts

--- __tests__/redirects.ts
+++ __tests__/redirects.ts
@@ -81,36 +81,42 @@ describe('meet.serlo.org', () => {
     const target = 'https://meet.google.com/fxn-iprp-ezx'
     expectToBeRedirectTo(response, target, 302)
   })
+
   test('meet.serlo.org/2 ', async () => {
     const response = await handleUrl('https://meet.serlo.local/2')
 
     const target = 'https://meet.google.com/yku-aksd-fkk'
     expectToBeRedirectTo(response, target, 302)
   })
+
   test('meet.serlo.org/3 ', async () => {
     const response = await handleUrl('https://meet.serlo.local/3')
 
     const target = 'https://meet.google.com/qma-zouf-vcz'
     expectToBeRedirectTo(response, target, 302)
   })
+
   test('meet.serlo.org/4 ', async () => {
     const response = await handleUrl('https://meet.serlo.local/4')
 
     const target = 'https://meet.google.com/iskddmh-wrh'
     expectToBeRedirectTo(response, target, 302)
   })
+
   test('meet.serlo.org/5 ', async () => {
     const response = await handleUrl('https://meet.serlo.local/5')
 
     const target = 'https://meet.google.com/yig-sxgw-zwx'
     expectToBeRedirectTo(response, target, 302)
   })
+
   test('meet.serlo.org/6 ', async () => {
     const response = await handleUrl('https://meet.serlo.local/6')
 
     const target = 'https://meet.google.com/icu-cubt-wgs'
     expectToBeRedirectTo(response, target, 302)
   })
+
   test('returns 404 when meet room is not defined', async () => {
     const response = await handleUrl('https://meet.serlo.local/def')
 
