700d66b3686eac0df668bd1d0dbbebb3f7efdb23
Stephan Kulla <git.mail@kulla.me>
2021-02-07 01:29:02 +0100

    fetch-helper: Add withdefaults()

--- __tests__/__utils__/fetch-helper.ts
+++ __tests__/__utils__/fetch-helper.ts
@@ -19,7 +19,6 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/serlo.org-cloudflare-worker for the canonical source repository
  */
-
 import { handleRequest } from '../../src'
 import { isInstance } from '../../src/utils'
 import {
@@ -29,16 +28,16 @@ import {
 } from './test-environment'
 
 export function fetchSerlo(spec: UrlSpec, init?: RequestInit) {
-  const enviornment = spec.environment ?? getTestEnvironment()
   const request = new Request(createUrl(spec), init)
+  const { environment, subdomain } = withDefaults(spec)
 
-  if (enviornment === TestEnvironment.Local) {
+  if (environment === TestEnvironment.Local) {
     return handleRequest(request)
   } else {
     // See https://github.com/mswjs/msw/blob/master/src/context/fetch.ts
     request.headers.set('x-msw-bypass', 'true')
 
-    if (enviornment === TestEnvironment.Staging && isInstance(spec.subdomain)) {
+    if (environment === TestEnvironment.Staging && isInstance(subdomain)) {
       request.headers.set('Authorization', 'Basic c2VybG90ZWFtOnNlcmxvdGVhbQ==')
     }
 
@@ -46,12 +45,9 @@ export function fetchSerlo(spec: UrlSpec, init?: RequestInit) {
   }
 }
 
-export function createUrl({
-  subdomain = '',
-  pathname = '/',
-  environment = getTestEnvironment(),
-  protocol = 'https',
-}: UrlSpec) {
+export function createUrl(spec: UrlSpec) {
+  const { protocol, subdomain, environment, pathname } = withDefaults(spec)
+
   return (
     protocol +
     '://' +
@@ -60,6 +56,15 @@ export function createUrl({
     domains[environment] +
     pathname
   )
+}
+
+function withDefaults(spec: UrlSpec): Required<UrlSpec> {
+  return {
+    subdomain: spec.subdomain ?? '',
+    pathname: spec.pathname ?? '/',
+    environment: spec.environment ?? getTestEnvironment(),
+    protocol: spec.protocol ?? 'https',
+  }
 }
 
 interface UrlSpec {
