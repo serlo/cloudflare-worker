0721163fd2dbcfa879d3834f0ddb82d5ab46b78d
Stephan Kulla <git.mail@kulla.me>
2021-09-07 11:14:41 +0200

    frontend: Remove unused line

--- src/frontend-proxy.ts
+++ src/frontend-proxy.ts
@@ -58,7 +58,6 @@ export async function frontendSpecialPaths(
   if (
     url.pathname.startsWith('/_next/') ||
     url.pathname.startsWith('/_assets/') ||
-    url.pathname.startsWith('/api/auth/') ||
     url.pathname.startsWith('/api/frontend/') ||
     url.pathname === '/___graphql'
   )
