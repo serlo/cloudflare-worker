92ba54e756fda004f78c2d380a378386cab7acf8
Jonas Keinholz <git@inyono.dev>
2021-12-15 16:13:56 +0100

    update license headers

--- __tests__/frontend-proxy.ts
+++ __tests__/frontend-proxy.ts
@@ -7,7 +7,7 @@
  * you may not use this file except in compliance with the License
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *    https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS
@@ -16,8 +16,8 @@
  * limitations under the License.
  *
  * @copyright Copyright (c) 2021 Serlo Education e.V.
- * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
- * @link      https://github.com/serlo-org/serlo.org-cloudflare-worker for the canonical source repository
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo/serlo.org-cloudflare-worker for the canonical source repository
  */
 import { Instance } from '../src/utils'
 import {
