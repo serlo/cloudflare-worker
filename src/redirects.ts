0a1867c60be52c4af1642137d2784e43e636b90d
Jonas Keinholz <git@inyono.dev>
2021-12-15 16:46:50 +0100

    move lenabi redirects into lenabi folder

--- src/redirects.ts
+++ src/redirects.ts
@@ -22,11 +22,11 @@
 import { isNil } from 'ramda'
 
 import {
-  Url,
   createNotFoundResponse,
+  getPathInfo,
   Instance,
   isInstance,
-  getPathInfo,
+  Url,
 } from './utils'
 
 const meetRedirects: Record<string, string | undefined> = {
@@ -146,30 +146,6 @@ export async function redirects(request: Request) {
 
         return url.toRedirect(301)
       }
-    }
-  }
-
-  // Redirects for the LENABI project
-  // TODO: Add tests for those redirects
-  if (url.subdomain === Instance.De) {
-    switch (url.pathnameWithoutTrailingSlash) {
-      case '/lenabi/metadata-api':
-        return Response.redirect(
-          'https://nbviewer.org/github/serlo/evaluations/blob/e5c8f1714f06d24d1c9d1da306911abe839dcf2d/src/Prototype%20of%20metadata%20API%20for%20serlo.org%20%28LENABI%29.ipynb',
-          302
-        )
-      case '/lenabi/data-wallet':
-        return Response.redirect('https://lenabi.serlo-staging.dev/wallet', 302)
-      case '/lenabi/user-journey':
-        return Response.redirect(
-          'https://frontend-git-lenabi-flow-serlo.vercel.app/',
-          302
-        )
-      case '/lenabi/sso':
-        return Response.redirect(
-          'https://frontend-git-keycloak-serlo.vercel.app/sso',
-          302
-        )
     }
   }
 
