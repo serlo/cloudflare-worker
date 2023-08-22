e18529be566d5841d1c4c9dce7bbf583286d41de
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-08-22 22:45:53 +0200

    feat(maintenance-mode): add possibility to bypass

--- src/maintenance.tsx
+++ src/maintenance.tsx
@@ -1,14 +1,18 @@
 import { h } from 'preact'
 
 import { Maintenance } from './ui'
-import { Url, createPreactResponse } from './utils'
+import { Url, createPreactResponse, getCookieValue } from './utils'
 
 export function maintenanceMode(request: Request) {
   // Set manually to true, if you want maintenance mode.
   // We don't need to put this var in a KV. See https://github.com/serlo/cloudflare-worker/issues/415
   const enabled = false
+  const bypassCookie = getCookieValue(
+    'bypassMaintenance',
+    request.headers.get('Cookie'),
+  )
 
-  if (!enabled) return null
+  if (!enabled || bypassCookie === '1') return null
 
   const url = Url.fromRequest(request)
 
