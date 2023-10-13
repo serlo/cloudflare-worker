2837231f34ba11972f9329c9aef3fa7b7d7b23a1
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-10-13 21:43:23 +0200

    refactor(auth): remove dead code, long time ago used for testing

--- src/auth.ts
+++ src/auth.ts
@@ -4,8 +4,7 @@ import { Instance, createJsonResponse, Url } from './utils'
 export function auth(request: Request, env: CFEnvironment): Response | null {
   return (
     authFrontendSectorIdentifierUriValidation(request, env) ||
-    authKratosIdentitySchema(request) ||
-    authKratosGithubJsonnet(request)
+    authKratosIdentitySchema(request)
   )
 }
 
@@ -101,27 +100,3 @@ function authKratosIdentitySchema(request: Request): Response | null {
   return createJsonResponse(schema)
 }
 
-function authKratosGithubJsonnet(request: Request): Response | null {
-  const url = Url.fromRequest(request)
-  if (url.subdomain !== '' || url.pathname !== '/auth/kratos-github.jsonnet') {
-    return null
-  }
-
-  const net = `
-local claims = {
-  email_verified: false
-} + std.extVar('claims');
-
-{
-  identity: {
-    traits: {
-      [if "email" in claims && claims.email_verified then "email" else null]: claims.email,
-    },
-  },
-}
-  `
-
-  return new Response(net, {
-    headers: { 'Content-Type': 'text/plain' },
-  })
-}
