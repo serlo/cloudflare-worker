db34502f6b2d04da6efaddeb982a83870f648062
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-10-13 21:55:52 +0200

    refactor(auth): remove dead code
    See https://github.com/serlo/infrastructure-modules-shared/pull/54

--- src/auth.ts
+++ src/auth.ts
@@ -2,12 +2,10 @@ import { CFEnvironment } from './cf-environment'
 import { Instance, createJsonResponse, Url } from './utils'
 
 export function auth(request: Request, env: CFEnvironment): Response | null {
-  return (
-    authFrontendSectorIdentifierUriValidation(request, env) ||
-    authKratosIdentitySchema(request)
-  )
+  return authFrontendSectorIdentifierUriValidation(request, env)
 }
 
+// TODO: remove if it's not used anymore
 function authFrontendSectorIdentifierUriValidation(
   request: Request,
   env: CFEnvironment,
@@ -36,67 +34,3 @@ function authFrontendSectorIdentifierUriValidation(
 
   return createJsonResponse(redirectUris)
 }
-
-function authKratosIdentitySchema(request: Request): Response | null {
-  const url = Url.fromRequest(request)
-  if (
-    url.subdomain !== '' ||
-    url.pathname !== '/auth/kratos-identity.schema.json'
-  ) {
-    return null
-  }
-  const schema = {
-    $id: 'https://serlo.org/auth/kratos-identity.schema.json',
-    $schema: 'http://json-schema.org/draft-07/schema#',
-    title: 'User',
-    type: 'object',
-    properties: {
-      traits: {
-        type: 'object',
-        properties: {
-          email: {
-            type: 'string',
-            format: 'email',
-            'ory.sh/kratos': {
-              credentials: { password: { identifier: true } },
-              verification: { via: 'email' },
-              recovery: { via: 'email' },
-            },
-          },
-          username: {
-            type: 'string',
-            'ory.sh/kratos': {
-              credentials: { password: { identifier: true } },
-            },
-            pattern: '^[\\w\\-]+$',
-            // TODO: check if minimal username length should be reintroduced or delete it
-            // minLength: 2,
-            maxLength: 32,
-          },
-          description: {
-            type: 'string',
-          },
-          motivation: {
-            type: 'string',
-          },
-          profile_image: {
-            type: 'string',
-          },
-          language: {
-            type: 'string',
-          },
-          interest: {
-            type: 'string',
-            // empty string is needed to support users that registered before it was made required
-            enum: ['parent', 'teacher', 'pupil', 'student', 'other', ''],
-          },
-        },
-        required: ['email', 'username', 'interest'],
-        additionalProperties: false,
-      },
-    },
-  }
-
-  return createJsonResponse(schema)
-}
-
