2839e23572538556273baf9f3139d753f371b755
Stephan Kulla <git.mail@kulla.me>
2023-11-20 18:59:03 +0100

    refactor: Remove unused createNotFoundResponse()

--- __tests__/__utils__/services/frontend.ts
+++ __tests__/__utils__/services/frontend.ts
@@ -1,13 +1,8 @@
-import { http, ResponseResolver } from 'msw'
+import { http, HttpResponse, ResponseResolver } from 'msw'
 
 import { getUuid } from './database'
 import { createUrlRegex, responseWithContentType } from './utils'
-import {
-  createJsonResponse,
-  createNotFoundResponse,
-  isInstance,
-  Url,
-} from '../../../src/utils'
+import { createJsonResponse, isInstance, Url } from '../../../src/utils'
 
 export function givenFrontend(resolver: ResponseResolver) {
   globalThis.server.use(
@@ -78,11 +73,11 @@ export function defaultFrontendServer(): ResponseResolver {
     } else if (isInstance(instance)) {
       const uuid = getUuid(instance, pathname.length > 0 ? pathname : '/')
 
-      if (uuid == null) return createNotFoundResponse()
+      if (uuid == null) return new HttpResponse(null, { status: 404 })
 
       content = uuid.content ?? ''
     } else {
-      return createNotFoundResponse()
+      return new HttpResponse(null, { status: 404 })
     }
 
     const body =
