e6eb7e64c0d609e3813582305fbed6e5383d2b6e
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-10-30 12:43:55 +0100

    test(asset-proxy): add negative tests

--- __tests__/asset-proxy.ts
+++ __tests__/asset-proxy.ts
@@ -1,6 +1,9 @@
 import { http } from 'msw'
 
-import { currentTestEnvironment } from './__utils__'
+import {
+  currentTestEnvironment,
+  expectIsPlaceholderResponse,
+} from './__utils__'
 
 beforeEach(() => {
   globalThis.server.use(
@@ -11,6 +14,11 @@ beforeEach(() => {
           'Set-Cookie':
             'sessionId=abc123; Expires=Wed, 09 Nov 2024 07:28:00 GMT; Path=/; Domain=whatever.org; Secure; HttpOnly; SameSite=None',
         },
+      })
+    }),
+    http.get('https://whatever.org/notimage', () => {
+      return new Response('', {
+        headers: { 'content-type': 'application/json' },
       })
     }),
   )
@@ -26,3 +34,42 @@ test('request to https://asset-proxy.serlo.org/src?url=* gets asset from url que
   expect(response.headers.get('content-type')).toBe('image/png')
   expect(response.headers.get('Set-Cookie')).toBeNull()
 })
+
+describe('returns placeholder', () => {
+  test('when url parameter is empty', async () => {
+    const response = await requestAsset('')
+
+    expectIsPlaceholderResponse(response)
+  })
+
+  test('when url is invalid', async () => {
+    const response = await requestAsset('42')
+
+    expectIsPlaceholderResponse(response)
+  })
+
+  test('when url query parameter is missing', async () => {
+    const response = await currentTestEnvironment().fetch({
+      subdomain: 'asset-proxy',
+      pathname: '/src',
+    })
+
+    expectIsPlaceholderResponse(response)
+  })
+
+  test('when response is not image', async () => {
+    const response = await requestAsset('https://whatever.org/notimage')
+
+    expectIsPlaceholderResponse(response)
+  })
+})
+
+async function requestAsset(
+  url: string,
+  env = currentTestEnvironment(),
+): Promise<Response> {
+  return await env.fetch({
+    subdomain: 'asset-proxy',
+    pathname: '/src?url=' + encodeURIComponent(url),
+  })
+}
