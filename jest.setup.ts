6426d938786cdeb1f3be337635516de6847a6086
Stephan Kulla <git.mail@kulla.me>
2025-01-27 00:22:13 +0100

    chore: Upgrade dependencies

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unassigned-import
 import '@testing-library/jest-dom'
 import { jest } from '@jest/globals'
-import { type Event as SentryEvent } from '@sentry/types'
+import { type Event as SentryEvent } from '@sentry/core'
 import * as cryptoNode from 'crypto'
 import { http } from 'msw'
 import { setupServer } from 'msw/node'
