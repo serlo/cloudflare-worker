509a4dcbe8320e4435845a342254fdea60e7f87f
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-02-07 17:07:26 +0100

    feat(auth): add hacky way to add required query param for vidis

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,43 @@
+import * as t from 'io-ts'
+
 import { Instance, Url, CFEnvironment } from './utils'
 
-export function auth(request: Request, env: CFEnvironment): Response | null {
-  return authFrontendSectorIdentifierUriValidation(request, env)
+export function auth(
+  request: Request,
+  env: CFEnvironment,
+): Promise<Response | null> | (Response | null) {
+  return (
+    authFrontendSectorIdentifierUriValidation(request, env) ||
+    hackyVidisAdditionalQueryParam(request)
+  )
+}
+
+// Remove when Kratos supports additional custom upstream parameters
+// See https://github.com/ory/kratos/issues/4293
+async function hackyVidisAdditionalQueryParam(
+  request: Request,
+): Promise<Response | null> {
+  const url = Url.fromRequest(request)
+  if (url.subdomain !== '' || url.pathname !== '/api/.ory/self-service/login') {
+    return null
+  }
+  const response = await fetch(url.href, request)
+  const body = await response.json()
+
+  if (
+    response.status === 422 &&
+    t.type({ redirect_browser_to: t.string }).is(body)
+  ) {
+    return new Response(
+      JSON.stringify({
+        ...body,
+        redirect_browser_to:
+          body.redirect_browser_to + '&vidis_idp_hint=Landessystem',
+      }),
+      response,
+    )
+  }
+  return null
 }
 
 // TODO: remove if it's not used anymore
