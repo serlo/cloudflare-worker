8f10891ed18d76ef0c7b78cf5b2902b7e995de82
Waldi <waldemar.popp@chancenwerk.org>
2026-03-05 13:46:52 +0100

    Update redirects.ts
    Update Partner display merger

--- src/redirects.ts
+++ src/redirects.ts
@@ -60,7 +60,7 @@ export function redirects(request: Request, env: CFEnvironment) {
       case '/imprint':
         return Response.redirect('https://de.serlo.org/legal', 301)
       case '/Partner':
-        return Response.redirect('https://chancenwerk.de/ueber-uns/unsere-partnerinnen/',301)
+        return Response.redirect('https://chancenwerk.de/ueber-uns/unsere-partnerinnen/', 301)
       case '/nutzungsbedingungen':
       case '/21654':
       case '/21654/nutzungsbedingungen-und-urheberrecht':
